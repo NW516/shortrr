619f7d336c984524f820170d7e48e60c2cb81e53
Nicole Wiegand <nwiegand05@yahoo.com>
2020-08-28 14:55:06 -0400

    Debugging heroku errors

---
+++ client/build/precache-manifest.fc43e7933e4119f9078d3349d296fb31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086a4fa8678f57727b2c935f11ab82c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11c710183804a1090703",
+    "url": "/static/css/main.fdb762e5.chunk.css"
+  },
+  {
+    "revision": "6c78c6c679aad993f7cd",
+    "url": "/static/js/2.078ebec7.chunk.js"
+  },
+  {
+    "revision": "5a425166da6b3ca2e5810e583d48622a",
+    "url": "/static/js/2.078ebec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c710183804a1090703",
+    "url": "/static/js/main.4597025c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "581ed24d85beca3d367474f782279891",
+    "url": "/static/media/links-bg.581ed24d.jpg"
+  }
+]);
