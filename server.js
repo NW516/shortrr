266e3edd740d59810776180e55a743f15248a352
Nicole Wiegand <nwiegand05@yahoo.com>
2020-08-28 10:49:37 -0400

    Debugging heroku errors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Add Express Port
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use((req, res, next) => {
